1eaa426a81c68b3b9745d16e9862bb69f5db3cb4
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-20 15:26:10 -0500

    commit tree e4d75e55cb74ce414ab38d952c80845d3a34d52a

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+    speed > 60 || speed < 40 ? true : false
+    speed <= 60 && speed >= 40 ? false : true
 end
 
 
 
 def not_safe?(speed)
-	
+    # use ternary statements
+    # return true if speed > 60 or < 40
+    speed > 60 || speed < 40 ? true : false
+    # return false if speed is between 40 and 60
+    speed <= 60 && speed >= 40 ? false : true
 end
 	
 
